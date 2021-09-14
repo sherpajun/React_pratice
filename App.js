197f8f034dff67ba077361aa2efe86bc0691e65d
sherpajun <87353476+sherpajun@users.noreply.github.com>
2021-09-14 17:59:59 +0900

    src
    New source file

--- App.js
+++ App.js
@@ -1,64 +1,36 @@
-import React from 'react';
-import './App.css';
-import PropTypes from 'prop-types';
-
-
-function Food({name, picture, rating}){
-  return (
-  <div>
-  <h2> I like {name} </h2>
-  <h4> {rating}/5.0</h4>
-  <img src={picture} alt={name} width="100"/>
-  </div>
-  );
-}
-const foodLike=[
-  {
-    id:1,
-    name:'kimchi',
-    image:'https://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating:5
-  },
-  {
-    id:2,
-    name:'sanjeok',
-    image:'https://aeriskitchen.com/wp-content/uploads/2008/09/Beef_SanJeok--55x55.jpg',
-    rating: 4.9
-  },
-  {
-    id:3,
-    name:'curry',
-    image:'https://aeriskitchen.com/wp-content/uploads/2008/09/Curry_and_Rice_01--55x55.jpg',
-    rating:2
-
-  },
-  {
-    id:4,
-    name:'black_bean_paste_noodles',
-    image:'https://aeriskitchen.com/wp-content/uploads/2008/09/black_bean_paste_noodles_04-1-1.jpg://aeriskitchen.com/wp-content/uploads/2008/09/Curry_and_Rice_01--55x55.jpg',
-    
-  }
-];//const는 상수
-//let foodLike=[];//let은 변수
-
-const renderFood=  dish=> <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>; 
-
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 
 function App() {
-  console.log(foodLike.map(renderFood))
   return (
-    <div>
-      <h1>Favorites Foods</h1>
-      {foodLike.map(renderFood)}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <DayList />
+          </Route>
+          <Route path="/day/:day">
+            <Day />
+          </Route>
+          <Route path="/create_word">
+            <CreateWord />
+          </Route>
+          <Route path="/create_day">
+            <CreateDay />
+          </Route>
+          <Route>
+            <EmptyPage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
-
-Food.prototype={
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  rating:PropTypes.number,
-};
-
 
 export default App;
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App4 from './App4';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App4 />,document.getElementById('root'));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
